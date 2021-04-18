03112046d2087db2c0e158458f667d661c467fbc
Sebastián Cueva <s.cueva78@gmail.com>
2021-04-18 14:31:53 +0200

    ultimos cambios

--- utilidades/utilidades.js
+++ utilidades/utilidades.js
@@ -98,60 +98,96 @@ function resetearJuego(){
     document.getElementById("boton-nueva-partida").disabled = false;
 }
 
+function mostrarCartasBanca(){
+    let cartas_borrosas = document.getElementsByClassName('carta-borrosa');
+
+    [].forEach.call(cartas_borrosas, function (borrosa) {
+       /* Mobile First imagenes: Seteado como ipad mini, se pueden añadir tantos condicionales como se desee */
+          borrosa.classList.remove("carta-borrosa");
+    })
+        /*for(var i = 0; i < cartas_borrosas.length; i++){
+            cartas_borrosas[i].classList.remove("carta-borrosa");
+        }*/
+   
+}
+
 function terminarJuego(){
+   
+
     if (jugador.puntuacion === 21) {
         document.getElementById("resultado").innerHTML = "¡Fantástico! Has conseguido un blackjack." + "<br>" + "pulsa en jugar si quieres otra partida";
         partida("ganaste");
         document.getElementById("dinero-jugador").innerHTML = "Tu dinero: $" + jugador.dinero;
         resetearJuego();
+       
     }
     if (jugador.puntuacion > 21) {
         document.getElementById("resultado").innerHTML = "¡Has pasado de 21! La banca gana" + "<br>" + "pulsa en jugar si quieres otra partida";
         partida("perdiste");
         document.getElementById("dinero-jugador").innerHTML = "Tu dinero: $" + jugador.dinero;
         resetearJuego();
+       
     }
     if (banca.puntuacion === 21) {
         document.getElementById("resultado").innerHTML = "Has perdido. La banca tiene un blackjack" + "<br>" + "pulsa en jugar si quieres otra partida";
         partida("perdiste");
         document.getElementById("dinero-jugador").innerHTML = "Tu dinero: $" + jugador.dinero;
         resetearJuego();
+        
     }
     if (banca.puntuacion > 21) {
         document.getElementById("resultado").innerHTML = "La banca se ha pasado de los 21 ¡Tu ganas!" + "<br>" + "pulsa en jugar si quieres otra partida";
         partida("ganaste");
         document.getElementById("dinero-jugador").innerHTML = "Tu dinero: $" + jugador.dinero;
         resetearJuego();
+        
     }
     if (banca.puntuacion >= 17 && jugador.puntuacion > banca.puntuacion && jugador.puntuacion < 21) {
         document.getElementById("resultado").innerHTML = "¡Has ganado! Tu puntuación supera a la de la banca." + "<br>" + "pulsa en jugar si quieres otra partida";
         partida("ganaste");
         document.getElementById("dinero-jugador").innerHTML = "Tu dinero: $" + jugador.dinero;
         resetearJuego();
+       
     }
     if (banca.puntuacion >= 17 && jugador.puntuacion < banca.puntuacion && banca.puntuacion < 21) {
         document.getElementById("resultado").innerHTML = "¡Perdiste! La banca te supera en puntuación" + "<br>" + "pulsa en jugar si quieres otra partida";
         partida("perdiste");
         document.getElementById("dinero-jugador").innerHTML = "Tu dinero: $" + jugador.dinero;
         resetearJuego();
+        
     }
     if (banca.puntuacion >= 17 && jugador.puntuacion === banca.puntuacion && banca.puntuacion < 21) {
         document.getElementById("resultado").innerHTML = "¡Empate!" + "<br>" + "pulsa en jugar si quieres otra partida";
         resetearJuego();
+        
     }
     if (jugador.dinero <= 0) {
         document.getElementById("boton-nueva-partida").disabled = true;
         document.getElementById("mas-cartas").disabled = true;
         document.getElementById("plantarse").disabled = true;
-        document.getElementById("resultado").innerHTML = "You lost!" + "<br>" + "You are out of money" + "<br>" + "<input type='button' value='New Game' onclick='location.reload();'/>";
+        document.getElementById("resultado").innerHTML = "¡Has perdido!" + "<br>" + "Estás sin dinero" + "<br>" + "<input type='button' value='Comenzar juego' onclick='location.reload();'/>";
     }
 }
 
 function cartasBanca(){
     banca.cartas.push(tablero.tableroArr[numeroCartasEnviadas]);
     banca.puntuacion = obtenerPuntuacion(banca.cartas);
-    document.getElementById("cartas-banca").innerHTML = "Cartas Banca: " + JSON.stringify(banca.cartas);
-    document.getElementById("puntuacion-banca").innerHTML = "Puntuacion Banca: " + banca.puntuacion;
+    //document.getElementById("cartas-banca").innerHTML = "Cartas Banca: " + JSON.stringify(banca.cartas);
+    document.getElementById("puntuacion-banca").innerHTML = "<h3>Puntuacion Banca:</h3> <h2>" + banca.puntuacion+"</h2>";
+    let cartasBanca = banca.cartas;
+    document.getElementById("cartas-banca").innerHTML = "";
+
+    for(var i = 0; i < cartasBanca.length; i++){
+        if(cartasBanca[i]){
+            console.log(`Carta ${i}: `,  `img/${cartasBanca[i].palo}_${cartasBanca[i].valor}.svg` );
+            let cartaFisica = document.createElement("div");
+            //cartaFisica.classList.add("carta-borrosa");
+            cartaFisica.classList.add("carta-fisica");
+            cartaFisica.style.backgroundImage = `url('img/${cartasBanca[i].palo}_${cartasBanca[i].valor}.svg')`;
+            document.getElementById("cartas-banca").append(cartaFisica);
+        }
+    }
+
     numeroCartasEnviadas += 1;
 }
 
@@ -161,6 +197,8 @@ function nuevaPartida(){
     document.getElementById("plantarse").disabled = false;
     document.getElementById("apuesta").disabled = true;
     document.getElementById("resultado").innerHTML = "";
+    document.getElementById("cartas-banca").innerHTML = "";
+    document.getElementById("cartas-jugador").innerHTML = "";
     nuevaCarta();
     nuevaCarta();
     cartasBanca();
@@ -170,8 +208,23 @@ function nuevaPartida(){
 function nuevaCarta(){
     jugador.cartas.push(tablero.tableroArr[numeroCartasEnviadas]);
     jugador.puntuacion = obtenerPuntuacion(jugador.cartas);
-    document.getElementById("cartas-jugador").innerHTML = "Cartas jugador: " + JSON.stringify(jugador.cartas);
-    document.getElementById("puntuacion-jugador").innerHTML = "Puntuacion jugador: " + jugador.puntuacion;
+    //document.getElementById("cartas-jugador").innerHTML = "Cartas jugador: " + JSON.stringify(jugador.cartas);
+    document.getElementById("puntuacion-jugador").innerHTML = "<h3>Puntuacion jugador:</h3> <h2>" + jugador.puntuacion+"</h2>";
+    let cartasEnJuego = jugador.cartas;
+    /*console.log("Valores", cartasEnJuego);
+    console.log("Total cartas", cartasEnJuego.length);
+    console.log("Carta 1: ",  `img/${cartasEnJuego[0].palo}_${cartasEnJuego[0].valor}.svg` );*/
+    document.getElementById("cartas-jugador").innerHTML = "";
+    for(var i = 0; i < cartasEnJuego.length; i++){
+        if(cartasEnJuego[i]){
+            console.log(`Carta ${i}: `,  `img/${cartasEnJuego[i].palo}_${cartasEnJuego[i].valor}.svg` );
+            let cartaFisica = document.createElement("div");
+            cartaFisica.classList.add("carta-fisica");
+            cartaFisica.style.backgroundImage = `url('img/${cartasEnJuego[i].palo}_${cartasEnJuego[i].valor}.svg')`;
+            document.getElementById("cartas-jugador").append(cartaFisica);
+        }
+    }
+    
     numeroCartasEnviadas += 1;
     if (numeroCartasEnviadas >= 2) {
         terminarJuego();
